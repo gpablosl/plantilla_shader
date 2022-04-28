0e7e8450321dd549310ab9a726423e614ae8b0bc
gpablosl <69810373+gpablosl@users.noreply.github.com>
2022-04-28 11:40:41 -0700

    Update plantilla_shader.py

--- plantilla_shader.py
+++ plantilla_shader.py
@@ -1,7 +1,9 @@
-from ctypes import c_void_p
 import OpenGL.GL as gl
 import glfw
 import numpy as np
+
+from pyexpat import model
+from re import M
 from Shader import *
 from Modelo import *
 
@@ -19,13 +21,17 @@ with open('fragment_shader.glsl') as archivo:
     fragment_shader_source = archivo.readlines()
 
 def dibujar():
+
     global modelo
+
     modelo.dibujar()
 
 def main():
     global modelo
+
     glfw.init()
 
+    # Trabaja con la versión 3
     glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR,3)
     glfw.window_hint(glfw.CONTEXT_VERSION_MINOR,3)
     glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
@@ -41,32 +47,32 @@ def main():
     glfw.make_context_current(window)
     glfw.set_framebuffer_size_callback(window, framebuffer_size_callbak)
 
-    shader  = Shader(vertex_shader_source, fragment_shader_source)
+    shader = Shader(vertex_shader_source, fragment_shader_source)
 
     posicion_id = gl.glGetAttribLocation(shader.shader_program, "position")
 
     modelo = Modelo(shader, posicion_id)
 
-
-    #draw loop
+    # Draw loop
     while not glfw.window_should_close(window):
         gl.glClearColor(0.3,0.3,0.3,1.0)
         gl.glClear(gl.GL_COLOR_BUFFER_BIT)
 
+        # Dibujar
+        dibujar()
+
         glfw.swap_buffers(window)
         glfw.poll_events()
 
-    #Liberar memoria
+    #Liberar Memoria
     modelo.borrar()
     shader.borrar()
-
+    
     glfw.terminate()
     return 0
 
 def framebuffer_size_callbak(window, width, height):
     gl.glViewport(0,0,width,height)
 
-
 if __name__ == '__main__':
     main()
-
