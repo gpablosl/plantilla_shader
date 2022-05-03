b848c993fd979f5958c0066849123cd9e77a6fa1
gpablosl <69810373+gpablosl@users.noreply.github.com>
2022-05-03 10:29:30 -0700

    Update Modelo.py

--- Modelo.py
+++ Modelo.py
@@ -3,15 +3,9 @@ import numpy as np
 from ctypes import c_void_p
 
 class Modelo:
-    def __init__(self, shader, posicion_id):
+    def __init__(self, shader, posicion_id, color_id):
         self.shader = shader
-        self.vertices = np.array(
-            [
-                -0.15,-0.5,0.0,  #izquierda, abajo
-                0.0, 0.5, 0.0,  #arriba
-                0.5, -0.5, 0.0  #derecha
-            ], dtype="float32"
-        )
+
 
         #Generar vertex array object y vertex buffer object
         self.VAO = gl.glGenVertexArrays(1)
@@ -25,9 +19,15 @@ class Modelo:
         gl.glBufferData(gl.GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, 
             gl.GL_STATIC_DRAW)
         #Definir como leer el VAO y activarlo
-        gl.glVertexAttribPointer(posicion_id, 3, gl.GL_FLOAT, 
-            gl.GL_FALSE, 0, c_void_p(0))
+        #posicion
+        gl.glVertexAttribPointer(posicion_id, 4, gl.GL_FLOAT, 
+            gl.GL_FALSE, 8 * self.vertices.itemsize, c_void_p(0))
         gl.glEnableVertexAttribArray(posicion_id)
+        #color
+        gl.glVertexAttribPointer(color_id, 4, gl.GL_FLOAT,
+            gl.GL_FALSE, 8 * self.vertices.itemsize, c_void_p(4 * self.vertices.itemsize))
+        gl.glEnableVertexAttribArray(color_id)
+
 
         gl.glBindBuffer(gl.GL_ARRAY_BUFFER, 0)
         gl.glBindVertexArray(0)
@@ -44,5 +44,4 @@ class Modelo:
     def borrar(self):
         gl.glDeleteVertexArrays(1, self.VAO)
         gl.glDeleteBuffers(1, self.VBO)
-
-    
+        
