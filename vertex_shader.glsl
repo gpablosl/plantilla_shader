b6788ba637a6ed67df671158e968728c3104b441
gpablosl <69810373+gpablosl@users.noreply.github.com>
2022-05-03 10:30:50 -0700

    Update vertex_shader.glsl

--- vertex_shader.glsl
+++ vertex_shader.glsl
@@ -1,8 +1,12 @@
 #version 330 core
-layout (location = 0) in vec3 position;
+layout (location = 0) in vec4 position;
+in vec4 color;
+
+//Los out van hacia el fragment shader
+out vec4 fragmentColor;
 //hay que establecer posicion en la propiedad
 //gl_Position que es del tipo vec4
 void main() {
-gl_Position = vec4(position.x, position.y,
-    position.z, 1.0);
+    gl_Position = position;
+    fragmentColor = color;
 }
